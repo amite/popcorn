9aff94e4bb7ad58e335588ae934d6a023a48bf70
elitenomad <stalin.pranava@gmail.com>
2014-05-08 20:15:23 +1000

    Angular + Rails Web app development

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Popcorn::Application.config.secret_key_base = 'd5b0f7ec7a15a955ddfc3bb732da632580351d34a7278fe9a897f85c0220acd63025c49a2301645fa44a2702eed0d05354dd795b478e0a599363fff039e45a75'
