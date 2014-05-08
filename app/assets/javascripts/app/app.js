4e9d548c77a4dd19194477cd5a279a72045fd941
elitenomad <stalin.pranava@gmail.com>
2014-05-08 21:02:56 +1000

    Completed Introduction and Setup

---
+++ app/assets/javascripts/app/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by pranavaswaroop on 8/05/2014.
+ */
+
+var App = angular.module('popcornApp',[]);
+
+App.controller("MoviesController",function($scope){
+    $scope.user = {
+        name: 'Pranava S Balugari'
+    }
+});
