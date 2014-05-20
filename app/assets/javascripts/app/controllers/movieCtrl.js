77142359e1661fff0048bf412a75dd85fb8ddfdb
elitenomad <stalin.pranava@gmail.com>
2014-05-20 10:25:07 +1000

    Corrected http to https in all the movie files

--- app/assets/javascripts/app/controllers/movieCtrl.js
+++ app/assets/javascripts/app/controllers/movieCtrl.js
@@ -19,7 +19,7 @@ App.controller("MovieController", function($scope, MoviesService,Movie, $routePa
     Movie.query({youtube_id: $routeParams.movie_id}).then(function(movies) {
         if(movies.length > 0) {
             var movie = movies[0];
-            movie.youtubeUrl = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + movie.youtubeId + "?rel=0");
+            movie.youtubeUrl = $sce.trustAsResourceUrl("https://www.youtube.com/embed/" + movie.youtubeId + "?rel=0");
             $scope.movie = movie;
         }
     });
